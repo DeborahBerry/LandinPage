30f000e184e0cd9d06b5f5d0cd76abbc7dfa3038
Deborah Berry <berrydebby20@gmail.com>
2024-05-11 01:35:21 +0300

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Main = () => {
+    return(
+        <div className="Main">
+            <div className="col col1">
+                <h2>Berry's cake</h2>
+                <p>Where every cake tells a story</p>
+                <button type="button">Order now</button>
+            </div>
+
+            <div className="col">
+                <div className="card card1"></div>
+                <div className="card card2"></div>
+                <div className="card card3"></div>
+                <div className="card card4"></div>
+                <div className="card card5"></div>
+                <div className="card card6"></div>
+            </div>
+        </div>
+    );
+}
+export default Main;
